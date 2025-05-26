54984d703b9cf2841b44c5982d3b784a59b360b3
J-Breno <joao.breno85@hotmail.com>
2025-05-26 12:05:44 -0300

    Implementar Relacionamentos OnetoMany e ManyToOne

--- 01-semana/course/src/main/java/com/github/jbreno/ead/course/repositories/ModuleRepository.java
+++ 01-semana/course/src/main/java/com/github/jbreno/ead/course/repositories/ModuleRepository.java
@@ -1,8 +1,9 @@
 package com.github.jbreno.ead.course.repositories;
 
+import com.github.jbreno.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ModuleRepository extends JpaRepository<Module, UUID> {
+public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
 }
