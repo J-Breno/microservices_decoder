3cac02fd754829ee3ef3b665150f9585502cf758
J-Breno <joao.breno85@hotmail.com>
2025-05-26 10:41:57 -0300

    Hipermídias com Spring Hateoas

--- 01-semana/authuser/src/main/java/com/github/jbreno/ead/authuser/models/UserModel.java
+++ 01-semana/authuser/src/main/java/com/github/jbreno/ead/authuser/models/UserModel.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.github.jbreno.ead.authuser.enums.UserStatus;
 import com.github.jbreno.ead.authuser.enums.UserType;
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -17,7 +18,7 @@ import java.util.UUID;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "TB_USERS")
-public class UserModel implements Serializable {
+public class UserModel extends RepresentationModel<UserModel> implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
