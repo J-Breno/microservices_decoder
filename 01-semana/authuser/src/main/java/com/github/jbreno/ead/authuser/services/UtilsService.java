6d8b383fa673b9c6205671f7dddece2441fd5713
J-Breno <joao.breno85@hotmail.com>
2025-05-27 05:20:55 -0300

    API Composition Pattern em Authuser Microservice – Parte 02

---
+++ 01-semana/authuser/src/main/java/com/github/jbreno/ead/authuser/services/UtilsService.java
@@ -0,0 +1,9 @@
+package com.github.jbreno.ead.authuser.services;
+
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public interface UtilsService {
+    public String createUrl(UUID userId, Pageable pageable);
+}
---
+++ 01-semana/authuser/src/main/java/com/github/jbreno/ead/authuser/services/impl/UtilServiceImpl.java
@@ -0,0 +1,17 @@
+package com.github.jbreno.ead.authuser.services.impl;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UtilServiceImpl {
+
+    String REQUEST_URI = "http://localhost:8082";
+
+    public String createUrl(UUID userId, Pageable pageable) {
+      return REQUEST_URI + "/courses?userId" + userId + "&page=" + pageable.getPageNumber() + "&size=" + pageable.getPageSize()
+                + "&sort=" + pageable.getSort().toString().replaceAll(": ", "m");
+    }
+}
